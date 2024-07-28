374b2d103a492652917b015e6f2f2d7e3cc40d5a
Dane Rutherford <danerutherford94@gmail.com>
2024-07-28 10:18:37 -0400

    Adding some basic functions to the calculator scripts.

---
+++ calculatorScripts.js
@@ -0,0 +1,25 @@
+function Add(num1, num2){
+    return num1 + num2;
+}
+
+function Subtract(num1, num2){
+    return num1 - num2;
+}
+
+function Multiply(num1, num2){
+    return num1 * num2;
+}
+
+function Divide(num1, num2){
+    return num1 / num2;
+}
+
+function Square(num1){
+    return num1 * 2
+}
+
+function Cube(num1){
+    return num1 * 3
+}
+
+
