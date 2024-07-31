be9081dc7423802402772a6487cec505d083d911
Dane Rutherford <danerutherford94@gmail.com>
2024-07-31 12:31:18 -0400

    Finalized styling, now working on functionality.

--- calculatorScripts.js
+++ calculatorScripts.js
@@ -1,3 +1,11 @@
+let btns = document.querySelectorAll('button');
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function() {
+    handleButtonClick(btn.textContent);
+  });
+});
+
 function Add(num1, num2){
     return num1 + num2;
 }
@@ -15,11 +23,13 @@ function Divide(num1, num2){
 }
 
 function Square(num1){
-    return num1 * 2
+    return num1 * num1
 }
 
-function Cube(num1){
-    return num1 * 3
+function Cube(num1){ 
+    return num1 * num1 * num1
 }
 
-
+function handleButtonClick(btn){
+    
+}
